537fd9dc59744c7334443732dcccaeacd04308c0
Runcy Oommen <runcy.oommen@gmail.com>
2017-12-26 17:10:11 +0530

    Files submitted as part of the prelim roundwq!

---
+++ prelim/todo.php
@@ -0,0 +1,112 @@
+ASK CHETALE TO SEND DOC...
+
+Change siteadmin password to admin?? or complex so that it can't be GTS..
+Change session_fetch url to much complex to test scripting skills.
+Evaluation and legal doc.
+
+
+Buy-Amount - limit enforcement via JS
+
+Share
+-POC for CSRF
+-POC for BlindSQLi
+
+- Changes in the LOGO
+
+- [https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Soci%C3%A9t%C3%A9_G%C3%A9n%C3%A9rale.svg/2000px-Soci%C3%A9t%C3%A9_G%C3%A9n%C3%A9rale.svg.png]
+
+
+
+
+Sell amount need to fixed - int to float [done]
+Layout issues on other options in member area. [fixed]
+
+
+Nice to have feature
+- complex password meter
+
+Buy Amount (rate limit)
+
+
+-- See update instead of create while editing the order. [fixed]
+-- delete function... [fixed]
+
+
+Roman* > Nishant will have feedback one, BasicAuth
+
+- SEC, SME, Roman, Prince...
+
+
+PASS: md5
+SQLi:...
+
+
+
+- After successful Edit, redirect user to New Order page.
+
+——————————————————————————————————————————————————————————————————————————————————————————
+
+- Enumerate the services running in the box.   (Enum - running)
+
+
+
+-  Find the web server’s version and the operating system of the box.
+    Apache 2.0
+    Ubuntu... / Ubuntu 14.04.5 LTS
+
+-  Find the fuzzy port number running SSH (1-65525)
+    2220
+
+    HEX MESSAGE
+
+-  Reveal all the common files/artifacts which provide information on the site architecture and overall structure;
+    robots.txt
+    includes Directory
+    phpinfo()
+    backup.zip
+    .git
+
+-  Describe the major vulnerability in the home page of the given website based on OWASP TOP 10
+    - https://www.owasp.org/index.php/Category:OWASP_Top_Ten_2017_Project
+
+    Home page leading to member area contains:
+
+    - SQL Injection / Content Injection
+    - Broken Auth / Session Management
+    - XSS
+    - Sec Misconfiguration
+    - Sensitive Data Exposure
+
+
+
+2.
+The intruder(hacker) should be able to gain access,(he can use any methodologies to do
+this– mainly through blind sql, session management etc) and hacker needs to Modify the
+placed orders
+
+
+
+The hacker should be able to:
+Find Login/logout timings of a particular user;
+
+- hacker should find Login/logout timings of a particular user
+- hack into all the Customer details
+- hack into all the Account details
+- Limit management (There is a limit set for a particular user/users, the hacker should be able to hack the
+system and modify the limit of a particular user or all the users)
+
+
+3.
+
+Bypass authentication in admin area and redirect site to hackerearth.com — Brute Force
+BasicAuth
+2. List any other common bug came across while on the site
+3. [Crypto/SQL Injection] After logging into the member area, perform the following functions:
+    - Find the master hash & cracked it
+    - Dump all user's
+    - Find the email ID and password of saved users
+
+
+
+
+- REMOVE BRUTEFORCE, FOCUS ON SESSION HIJACKING..
